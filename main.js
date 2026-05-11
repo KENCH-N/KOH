04b796c294763447dd20f95e91237f020d8f8bba
KENCH-N <151045596+KENCH-N@users.noreply.github.com>
2026-05-11 18:51:50 +0000

    main and stuff

--- main.js
+++ main.js
@@ -8,16 +8,30 @@ function main() {
         // Remove button after animation completes
         setTimeout(() => {
             button.remove();
-            route1();
+            route("Hello", "World", "!", document.getElementById("button1"), document.getElementById("button2"));
         }, 600); // Match the animation duration
     });
 }
 
-function route1() {
+function route(text1, text2, text3, button1, button2) {
     var p1 = document.getElementById("p1");
-    p1.innerText = "Hello";
-    p1.offsetHeight; // force reflow
+    var p2 = document.getElementById("p2");
+    var p3 = document.getElementById("p3");
+    
+    // Fade in each paragraph with a delay
     p1.classList.add('fade-in');
+    p1.innerText = "Hello";
+    p1.classList.remove('gone');
+    setTimeout(() => {
+        p2.classList.add('fade-in');
+        p2.innerText = "World";
+        p2.classList.remove('gone');
+    }, 600);
+    setTimeout(() => {
+        p3.classList.add('fade-in');
+        p3.innerText = "!";
+        p3.classList.remove('gone');
+    }, 1200);
 }
 
 main();
